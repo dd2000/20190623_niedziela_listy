d0dc1a8a39ca42db41afb01e52e82913461cf2bb
Dariusz.Żulpo <dd2000@wp.pl>
2019-06-23 09:28:42 +0200

    Listy - niedziela_powtórka new trener

---
+++ src/main/java/Zadanie_1/MyList.java
@@ -0,0 +1,18 @@
+package Zadanie_1;
+/*
+Dodaj klasę MojaLista (lub MyList), która będzie implementowała
+listę i posiadała następujące funkcjonalności:
+● dodawanie elementu na końcu listy
+● dodawanie elementu na wybrany indeks w liście
+● usuwanie elementu z listy
+● usuwanie elementu z listy na podstawie indeksu
+● pobieranie elementu na podstawie indeksu
+● szukanie elementu na liście
+ */
+
+public class MyList {
+    private String elementData;
+    private long next;
+    private long head; //głowa listy
+
+} // class MyList
